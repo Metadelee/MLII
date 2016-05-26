0aa55f8dae9c65911c97e4053c372b16bb332422
Tobias Graf <MobiTobi@users.noreply.github.com>
2016-05-26 22:26:20 +0200

    added test.py

--- ex03/test.py
+++ ex03/test.py
@@ -5,7 +5,7 @@ from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 import numpy as np
 from load import mnist
 
-srng = RandomStreams(use_cuda = True)
+srng = RandomStreams()
 
 def floatX(X):
     return np.asarray(X, dtype=theano.config.floatX)
@@ -14,7 +14,8 @@ def init_weights(shape):
     return theano.shared(floatX(np.random.randn(*shape) * 0.01))
 
 def init_slope(shape):
-    return theano.shared(floatX(0.25 + np.zeros((1,shape))),                        broadcastable=(True, False))
+    return theano.shared(floatX(0.25 + np.zeros((1,shape))),
+            broadcastable=(True, False))
 
 def rectify(X):
     return T.maximum(X, 0.)
@@ -38,19 +39,13 @@ def RMSprop(cost, params, lr=0.001, rho=0.9, epsilon=1e-6):
         updates.append((p, p - lr * g))
     return updates
 
-def dropout(X, p=1):
-    mask = srng.binomial(size=X.shape, p=p, dtype='floatX')
-    return X * mask / p
-
-
 def model(X, w_h, w_h2, w_o, s_h, s_h2, p_use_input, p_use_hidden):
-    #X = dropout(X, p_use_input)
+    #X = dropout(X, p_drop_input)
     #h = rectify(T.dot(X, w_h))
-    h = PRelu(T.dot(X, w_h), s_h)
-    #h = dropout(h, p_use_hidden)
-    #h2 = rectify(T.dot(h, w_h2))
-    h2 = PRelu(T.dot(X, w_h2), s_h2)
-    h2 = dropout(h2, p_use_hidden)
+    h = PRelu(T.dot(X, w_h), s_h )
+    #h = dropout(h, p_drop_hidden)
+    h2 = PRelu(T.dot(h, w_h2), s_h2)
+    #h2 = dropout(h2, p_drop_hidden)
     py_x = softmax(T.dot(h2, w_o))
     return h, h2, py_x
 
@@ -71,13 +66,13 @@ h, h2, py_x = model(X, w_h, w_h2, w_o, s_h, s_h2, 1., 1.)
 y_x = T.argmax(py_x, axis=1)
 
 cost = T.mean(T.nnet.categorical_crossentropy(noise_py_x, Y))
-params = [w_h, w_h2, w_o,]
+params = [w_h, w_h2, w_o, s_h]
 updates = RMSprop(cost, params, lr=0.001)
 
 train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
 predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
 
-for i in range(100): #you can adjust this if training takes too long
+for i in range(3): #you can adjust this if training takes too long
     for start, end in zip(list(range(0, len(trX), 128)), list(range(128, len(trX), 128))):
         cost = train(trX[start:end], trY[start:end])
     print(np.mean(np.argmax(teY, axis=1) == predict(teX)))
